89aa50a79cbf4306cd0742562abf67945be1eaaa
ashish10677 <ashish10677@gmail.com>
2020-04-06 01:02:53 +0530

    Add dropdown with dynamic data fetched from blockchain.
    Signed-off-by: ashish10677 <ashish10677@gmail.com>

--- election-client/src/App.js
+++ election-client/src/App.js
@@ -1,40 +1,66 @@
 import React from 'react';
 import './App.css';
 import ElectionBridge from './bridge/index';
+import { Select } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Option } = Select;
 
 class App extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      candidateList: [],
+      selectedCandidate: null
+    }
+  }
 
   componentDidMount() {
     console.log("Component mounted");
     let electionBridge = new ElectionBridge();
     electionBridge.getAllCandidates().then(res => {
-      console.log(res);
       let promise = [];
-      for(let i in res) {
+      for (let i in res) {
         promise.push(electionBridge.getCandidate(res[i]));
       }
       return Promise.all(promise);
     }).then(candidates => {
       console.log(candidates);
+      let candidateList = [];
+      for (let i in candidates) {
+        candidateList.push({
+          key: candidates[i].id,
+          voteCount: candidates[i].voteCount,
+          name: candidates[i].name
+        })
+      }
+      this.setState({
+        candidateList
+      })
     })
-    .catch(err => {
-      console.log(err);
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  getDropdownList = () => {
+    return this.state.candidateList.map((candidate) => <Option value={candidate.key}>{candidate.name}</Option>)
+  }
+
+  selectCandidate = (value) => {
+    this.setState({
+      selectedCandidate: value
     })
   }
 
   render() {
     return (
-      <div>
-        <header>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-            Learn React
-        </header>
+      <div className="App-header">
+        <h1>Vote Now</h1>
+        <Select style={{width: 250}} onChange={this.selectCandidate} placeholder="Select candidate">
+          {this.getDropdownList()}
+        </Select>
       </div>
     );
   }
